38ccb539110786c3fbcaaaba6d10a524385afa0a
Anand Pant <theapant@gmail.com>
2013-05-12 20:00:44 -0500

    Initial Bringup

---
+++ BoardConfig.mk
@@ -0,0 +1,171 @@
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+#USE_CAMERA_STUB := true
+
+
+# inherit from the proprietary version
+-include vendor/samsung/ancora_tmo/BoardConfigVendor.mk
+
+
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/ancora_tmo/include
+
+
+TARGET_NO_BOOTLOADER := true
+
+TARGET_BOARD_PLATFORM := msm7x30
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
+
+TARGET_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DQCOM_ROTATOR_KERNEL_FORMATS
+#TARGET_GLOBAL_CFLAGS += -DBINDER_COMPAT -DSAMSUNG_CAMERA_QCOM
+#TARGET_GLOBAL_CFLAGS += -DWITH_QCOM_LPA
+
+TARGET_BOOTLOADER_BOARD_NAME := ancora_tmo
+TARGET_OTA_ASSERT_DEVICE := ancora_tmo,SGH-T679,ancora_tmo
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_NO_KERNEL := false
+TARGET_PROVIDES_INIT_TARGET_RC := true
+TARGET_NO_INITLOGO := true
+#TARGET_NO_RECOVERY := true
+
+
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+WPA_SUPPLICANT_VERSION      := VER_0_6_X
+BOARD_WLAN_DEVICE           := bcm4329
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcm4329.ko"
+WIFI_DRIVER_FW_STA_PATH     := "/vendor/firmware/fw_bcm4329.bin"
+WIFI_DRIVER_FW_AP_PATH      := "/vendor/firmware/fw_bcm4329_apsta.bin"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/proc/calibration"
+WIFI_DRIVER_MODULE_NAME     := "bcm4329"
+
+
+BOARD_KERNEL_BASE := 0x00400000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_CMDLINE := console=ttyFIQ0 no_console_suspend
+
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+TARGET_NEEDS_BLUETOOTH_INIT_DELAY := true
+
+BOARD_MOBILEDATA_INTERFACE_NAME = "pdp0"
+
+BOARD_USES_LIBSECRIL_STUB := true
+
+BOARD_USES_GENERIC_AUDIO := true
+#BOARD_USES_QCOM_AUDIO_RESETALL := true
+#TARGET_USES_QCOM_LPA := true
+#BOARD_HAVE_SAMSUNG_AUDIO := true
+
+# FM Radio
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+BOARD_FM_DEVICE := si4709
+
+BOARD_EGL_CFG := device/samsung/ancora_tmo/egl.cfg
+
+USE_OPENGL_RENDERER := true
+
+BOARD_USE_SKIA_LCDTEXT := true
+
+BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
+
+#TARGET_USES_C2D_COMPOSITION := false
+#TARGET_USES_OVERLAY := false
+#TARGET_USES_SF_BYPASS := false
+#TARGET_HAVE_BYPASS := false
+#TARGET_GRALLOC_USES_ASHMEM := false
+#TARGET_USES_GENLOCK := true
+#TARGET_FORCE_CPU_UPLOAD := true
+
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QCOM_GPS := true
+BOARD_USES_QCOM_LIBS := true
+BOARD_USES_QCOM_LIBRPC := true
+BOARD_USES_QCOM_PMEM := true
+
+
+# Camera
+USE_CAMER_STUB := false
+ifeq ($(USE_CAMERA_STUB),false)
+BOARD_CAMERA_LIBRARIES := libcamera
+endif
+
+BOARD_USE_LEGACY_TOUCHSCREEN := true
+
+BOARD_VENDOR_QCOM_AMSS_VERSION := 6225
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := ancora
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 3200
+
+TARGET_USERIMAGES_USE_EXT4 := true
+
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_VOLD_MAX_PARTITIONS := 28
+
+BOARD_MTP_DEVICE := "/dev/usb_mtp_gadget"
+
+
+
+# Begin recovery stuff
+#
+# Partition sizes must match your phone, or all hell will break loose!
+# For the Galaxy W, these are calculated from /proc/partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 5767168
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 7864320
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 560988160
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1163919360
+BOARD_CACHEIMAGE_PARTITION_SIZE := 62914560
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_SDCARD_INTERNAL := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/ancora_tmo/recovery/recovery_ui.c
+BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/ancora_tmo/recovery/graphics.c
+TARGET_RECOVERY_INITRC := device/samsung/ancora_tmo/config/init.recovery.rc
+#BOARD_UMS_LUNFILE := /sys/devices/platform/usb_mass_storage/lun0/file
+TARGET_RECOVERY_UI_LIB := librecovery_ui_ancora
+# End recovery stuff
+
+#Kernel 
+
+TARGET_PREBUILT_KERNEL := device/samsung/ancora_tmo/prebuilt/zImage
+TARGET_KERNEL_CONFIG := ancora_tmo_rev00_defconfig
+
+
+#TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/usb_mass_storage/lun0/file
---
+++ camerahal/1Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := cameraHAL.cpp
+LOCAL_C_INCLUDES := $(TOP)/frameworks/base/include
+
+LOCAL_SHARED_LIBRARIES := liblog libutils libcutils
+LOCAL_SHARED_LIBRARIES += libui libhardware libcamera_client
+LOCAL_SHARED_LIBRARIES += libseccameraadaptor
+LOCAL_PRELINK_MODULE := false
+
+include $(BUILD_SHARED_LIBRARY)
